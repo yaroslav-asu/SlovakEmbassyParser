08b03fb10449c2df83f11aa1f92cd3789626766f
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-03-08 12:53:24 +0200

    fix: removed todo, removed not working functions

--- parser/city.go
+++ parser/city.go
@@ -90,41 +90,6 @@ func (p *Parser) CheckEmbassyWork(city models.City) string {
 	return "yes"
 }
 
-func (p *Parser) UpdateCitiesWithWorkingEmbassies() {
-	zap.L().Info("Started updating topicalCities with embassies in Db")
-	topicalCities := p.CitiesWithWorkingEmbassy()
-	zap.L().Info("Successfully got topicalCities with embassies")
-	for _, city := range topicalCities {
-		zap.L().Info("Trying to find or creating city with name: " + city.Name + " and id: " + city.Id + " in Db")
-		cityCopy := city
-		// TODO fix error: when city soft deleted from Db, gorm tries to create new one and gets error
-		record := p.Db.FirstOrCreate(&cityCopy)
-		if record.RowsAffected == 0 {
-			zap.L().Info("City with name:" + city.Name + " and id: " + city.Id + " in Db doesn't match with current, updating")
-			record.Save(&city)
-		}
-		zap.L().Info("City with name:" + city.Name + " and id: " + city.Id + " up to date")
-	}
-	p.DeleteOutdatedCities(topicalCities)
-}
-
-func (p *Parser) DeleteOutdatedCities(topicalCities []models.City) {
-	zap.L().Info("Starting to delete outdated cities")
-	topicalCitiesMap := make(map[string]bool)
-	for _, city := range topicalCities {
-		topicalCitiesMap[city.Id] = false
-	}
-	var dbCities []models.City
-	p.Db.Find(&dbCities)
-	for _, city := range dbCities {
-		_, found := topicalCitiesMap[city.Id]
-		if !found {
-			zap.L().Info(city.Name + " with id: " + city.Id + "no longer contain an embassy, deleting from Db")
-			p.Db.Delete(&city)
-		}
-	}
-}
-
 func (p *Parser) CitiesWithWorkingEmbassy() []models.City {
 	var workingCities []models.City
 	p.Db.Find(&workingCities)
