95ccc4448cc90f723f531c3a0bfd29b45abadc01
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-04-18 13:53:03 +0200

    feat: dumb parsing

---
+++ parser/reserve_requests.go
@@ -0,0 +1,51 @@
+package parser
+
+import (
+	"fmt"
+	gorm_models "main/models/gorm"
+	"main/models/gorm/datetime"
+	"main/parser/user"
+)
+
+func (p *Parser) RunCheckingReserveRequests() {
+	var userModel gorm_models.User
+	p.DB.DB.Where("id = 1").First(&userModel)
+	mainUser := user.NewUserFromModel(userModel)
+	for {
+		currentDate := datetime.NewDateYM(2023, 6)
+		for ; currentDate.Year() <= 2023 && currentDate.Month() <= 7; currentDate.MoveMonth(1) {
+			var city gorm_models.City
+			city = gorm_models.City{
+				Name: "Moscow",
+				Id:   "590",
+			}
+			fmt.Println(currentDate.Format(datetime.DateTime), city.Name)
+			for _, dayCell := range p.GetWorkingDaysInMonth(city, currentDate) {
+				if dayCell.AvailableReservations > 0 {
+					availableReservations, _ := p.GetReservations(city, currentDate)
+					for _, reservation := range availableReservations {
+						mainUser.ReserveDatetime(city, reservation.Date)
+					}
+				}
+			}
+		}
+		currentDate.MoveMonth(-1)
+		for ; currentDate.Year() >= 2023 && currentDate.Month() >= 6; currentDate.MoveMonth(-1) {
+			var city gorm_models.City
+			city = gorm_models.City{
+				Name: "Saint Petersburg",
+				Id:   "601",
+			}
+			fmt.Println(currentDate.Format(datetime.DateTime), city.Name)
+
+			for _, dayCell := range p.GetWorkingDaysInMonth(city, currentDate) {
+				if dayCell.AvailableReservations > 0 {
+					availableReservations, _ := p.GetReservations(city, currentDate)
+					for _, reservation := range availableReservations {
+						mainUser.ReserveDatetime(city, reservation.Date)
+					}
+				}
+			}
+		}
+	}
+}
