ebf12f1d3b7b3d1aaf6349fe70b4210d28d5e371
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-04-21 18:37:55 +0200

    fix: small fixes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	funcs.Init()
-	p := parser.NewParser()
-	p.RunCheckingReserveRequests()
+	siteParser := parser.NewParser()
+	siteParser.RunCheckingReserveRequests()
+	defer siteParser.Deconstruct()
 }
