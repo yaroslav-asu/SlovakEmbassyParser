900b3a39490e1ad215e6ab0220f8672d2e39dc48
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-04-21 18:37:55 +0200

    fix: small fixes

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"main/internal/session"
 	"main/internal/utils/funcs"
 )
 
 func main() {
 	funcs.Init()
-	s := session.NewLoggedInSession("herytlndten", "7753224")
-	s.SolveNewCaptcha()
 }
