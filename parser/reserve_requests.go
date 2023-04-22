99054e2f9ea51a5ac78f7fc34d726a3f14efdcc1
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-04-22 09:53:03 +0200

    feat: finishing work if user has been reserved fix: refactor code

--- parser/reserve_requests.go
+++ parser/reserve_requests.go
@@ -1,30 +1,32 @@
 package parser
 
 import (
-	"fmt"
 	gorm_models "main/models/gorm"
 	"main/models/gorm/datetime"
 	"main/parser/user"
+	"os"
 )
 
 func (p *Parser) RunCheckingReserveRequests() {
 	var userModel gorm_models.User
 	p.DB.Where("id = 1").First(&userModel)
 	mainUser := user.NewUserFromModel(userModel)
-	for {
+	for !mainUser.DB.IsReserved {
 		currentDate := datetime.NewDateYM(2023, 6)
-		for ; currentDate.Year() <= 2023 && currentDate.Month() <= 7; currentDate.MoveMonth(1) {
+		for ; currentDate.Year() <= 2023 && currentDate.Month() <= 8; currentDate.MoveMonth(1) {
 			var city gorm_models.City
 			city = gorm_models.City{
 				Name: "Moscow",
 				Id:   "590",
 			}
-			fmt.Println(currentDate.Format(datetime.DateTime), city.Name)
 			for _, dayCell := range p.GetWorkingDaysInMonth(city, currentDate) {
+				p.DB.Preload("City").Find(&dayCell)
 				if dayCell.AvailableReservations > 0 {
-					availableReservations, _ := p.GetReservations(city, currentDate)
+					availableReservations, _ := p.GetReservations(city, dayCell.Date)
 					for _, reservation := range availableReservations {
-						mainUser.ReserveDatetime(city, reservation.Date)
+						if mainUser.ReserveDatetime(city, reservation.Date) {
+							os.Exit(1)
+						}
 					}
 				}
 			}
@@ -36,13 +38,14 @@ func (p *Parser) RunCheckingReserveRequests() {
 				Name: "Saint Petersburg",
 				Id:   "601",
 			}
-			fmt.Println(currentDate.Format(datetime.DateTime), city.Name)
-
 			for _, dayCell := range p.GetWorkingDaysInMonth(city, currentDate) {
+				p.DB.Preload("City").Find(&dayCell)
 				if dayCell.AvailableReservations > 0 {
-					availableReservations, _ := p.GetReservations(city, currentDate)
+					availableReservations, _ := p.GetReservations(city, dayCell.Date)
 					for _, reservation := range availableReservations {
-						mainUser.ReserveDatetime(city, reservation.Date)
+						if mainUser.ReserveDatetime(city, reservation.Date) {
+							os.Exit(1)
+						}
 					}
 				}
 			}
