3e39834316631797f6fac8a4532c6c7cbf168803
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-04-29 19:40:14 +0200

    feat: connected ai model that solves captchas fix: changed solveCaptcha function

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"main/internal/utils/funcs"
-	"main/parser"
 )
 
 func main() {
 	funcs.Init()
-	siteParser := parser.NewParser()
-	siteParser.RunCheckingReserveRequests()
-	defer siteParser.Deconstruct()
 }
