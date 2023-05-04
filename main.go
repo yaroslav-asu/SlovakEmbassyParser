0f706625c806819f3743b77ee089f193c4fb4e60
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-05-04 11:25:25 +0200

    fix: session logic moved to session package

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"main/internal/utils/funcs"
-	"main/models/gorm/datetime"
 	"main/parser"
 )
 
 func main() {
 	funcs.Init()
 	p := parser.NewLoggedInParser()
-	date, err := p.CurrentMonth()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(date.Format(datetime.DateTime))
+	p.StartReserveRequestsParsing()
 }
--- parser/month.go
+++ parser/month.go
@@ -1,73 +1,18 @@
 package parser
 
 import (
-	"github.com/anaskhan96/soup"
 	"go.uber.org/zap"
 	"main/internal/utils/funcs"
 	gorm_models "main/models/gorm"
 	"main/models/gorm/datetime"
-	"math"
 	"strconv"
-	"strings"
 )
 
-func (p *Parser) monthDate(root soup.Root) (datetime.Date, error) {
-	monthDate := root.Find("td", "class", "calendarMonthLabel")
-	if monthDate.Error != nil {
-		return datetime.Date{}, monthDate.Error
-	}
-	return datetime.ParseDateFromString(datetime.SiteMonthYear, strings.Split(funcs.StripString(monthDate.FullText()), " ")[0])
-}
-
-func (p *Parser) CurrentMonth() (datetime.Date, error) {
-	root := p.stepToMonth(gorm_models.City{Id: "601", Name: "Saint-Petersburg"}, 0)
-	date, err := p.monthDate(root)
-	if err != nil {
-		zap.L().Error("Failed to get current date")
-		return datetime.Date{}, err
-	}
-	return date, nil
-}
-
-func (p *Parser) stepToMonth(city gorm_models.City, delta int) soup.Root {
-	var res soup.Root
-	if delta == 0 {
-		res = p.Session.GetParsedSoup(funcs.Linkify("calendar.do?consularPost=", city.Id))
-	} else {
-		res = p.Session.GetParsedSoup(funcs.Linkify("calendar.do?month=", strconv.Itoa(delta), "&consularPost=", city.Id))
-	}
-	p.Date.MoveMonth(delta)
-	return res
-}
-
-func (p *Parser) moveToMonth(city gorm_models.City, date datetime.Date) []soup.Root {
-	zap.L().Info("Starting move to month: " + date.Format(datetime.MonthAndYear) + " in city: " + city.Format())
-	monthMoveCount := date.Month() - p.Date.Month() + (date.Year()-p.Date.Year())*12
-	var responses []soup.Root
-	delta := 0
-	if monthMoveCount != 0 {
-		delta = int(math.Copysign(1, float64(monthMoveCount)))
-		for ; monthMoveCount != 0; monthMoveCount -= delta {
-			responses = append(responses, p.stepToMonth(city, delta))
-		}
-	} else {
-		responses = []soup.Root{p.stepToMonth(city, 0)}
-	}
-	zap.L().Info("Successfully moved to " + date.Format(datetime.MonthAndYear) + " in city: " + city.Format())
-	return responses
-}
-
-func (p *Parser) getMonthSoup(city gorm_models.City, date datetime.Date) soup.Root {
-	zap.L().Info("Starting getting month")
-	responses := p.moveToMonth(city, date)
-	return responses[len(responses)-1]
-}
-
-func (p *Parser) getWorkingDaysInMonth(city gorm_models.City, date datetime.Date) []gorm_models.DayCell {
+func (p *Parser) monthWorkDays(city gorm_models.City, date datetime.Date) []gorm_models.DayCell {
 	zap.L().Info("Started to get day cells")
 	funcs.Sleep()
 	var dayCells []gorm_models.DayCell
-	res := p.getMonthSoup(city, date)
+	res := p.Session.GetMonthSoup(city, date)
 	monthCell := res.FindAll("td", "class", "calendarMonthCell")
 	for _, el := range monthCell {
 		freeSpaceNode := el.Find("font")
@@ -97,7 +42,7 @@ func (p *Parser) getWorkingDaysInMonth(city gorm_models.City, date datetime.Date
 }
 
 func (p *Parser) DayCellsWithReservationsInMonth(city gorm_models.City, date datetime.Date) []gorm_models.DayCell {
-	dayCells := p.getWorkingDaysInMonth(city, date)
+	dayCells := p.monthWorkDays(city, date)
 	var DayCellsWithReservations []gorm_models.DayCell
 	for _, dayCell := range dayCells {
 		if dayCell.AvailableReservations > 0 {
