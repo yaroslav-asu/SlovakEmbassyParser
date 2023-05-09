91f44549770713caf2ff121661a92b8aeac68349
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-05-09 10:59:08 +0200

    fix: added captcha dirs to init

--- internal/session/captcha/init.go
+++ internal/session/captcha/init.go
@@ -6,9 +6,12 @@ import (
 )
 
 func Init() {
-	if _, err := os.Stat("captcha"); err != nil {
-		if err := os.Mkdir("captcha", os.ModePerm); err != nil {
-			zap.L().Fatal("Failed to create folder captcha")
+	dirs := []string{"captcha", "captcha/right", "captcha/wrong"}
+	for _, dirName := range dirs {
+		if _, err := os.Stat(dirName); err != nil {
+			if err := os.Mkdir(dirName, os.ModePerm); err != nil {
+				zap.L().Fatal("Failed to create folder: " + dirName)
+			}
 		}
 	}
 }
